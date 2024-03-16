fda7a9a6b0ab2b845caa2b646b1c531a15bdeefc
Kuba Karpierz <admiral-akk@users.noreply.github.com>
2024-03-16 12:36:05 -0400

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import glsl from "vite-plugin-glsl";
+
+export default {
+  optimizeDeps: {
+    include: ["linked-dep"],
+  },
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true, // Open to local network and display URL
+    open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
+  },
+  build: {
+    commonjsOptions: {
+      include: [/linked-dep/, /node_modules/],
+    },
+    outDir: "../dist", // Output in the dist/ folder
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true, // Add sourcemap
+  },
+  plugins: [glsl()],
+};
